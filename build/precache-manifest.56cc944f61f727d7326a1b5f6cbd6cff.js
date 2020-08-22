c86f0a0dd7e197c75c591e74c3743fc352e6e9f8
sabrina <krkoc86@gmail.com>
2020-08-22 12:55:12 -0300

    first commit

---
+++ build/precache-manifest.56cc944f61f727d7326a1b5f6cbd6cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cb6413b0a351325f180844feb79ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a14cd5123029feb665d1",
+    "url": "/static/css/main.d29b11cf.chunk.css"
+  },
+  {
+    "revision": "f5f336f426691ab6d2e8",
+    "url": "/static/js/2.a838f860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a838f860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14cd5123029feb665d1",
+    "url": "/static/js/main.2654d065.chunk.js"
+  },
+  {
+    "revision": "41d62c86f2d3a97341dd",
+    "url": "/static/js/runtime-main.3f28783b.js"
+  }
+]);
